309300316d6f95cbd1c1f363bd4a19e43dc790ff
dasarisaiteja <dasarisaiteja210@gmail.com>
2026-02-11 12:42:52 +0530

    updated header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectCartTotal } from '../utils/cartSlice';
+
+
+function Header() {
+  // We call our selector to get the total number of items
+  const cartTotal = useSelector(selectCartTotal);
+
+  function showCartBadge() {
+    if (cartTotal > 0) {
+      return <span className="cart-badge">{cartTotal}</span>;
+    }
+    // If cart is empty, return nothing
+    return null;
+  }
+
+  return (
+    <header className="my-header-bar">
+      <div className="header-container">
+        
+        <Link to="/" className="my-logo">
+          <h1>🛒 ShoppyGlobe</h1>
+        </Link>
+        
+        <nav className="navigation-menu">
+          <Link to="/" className="nav-item">Home</Link>
+          
+          <Link to="/cart" className="nav-item cart-button">
+            Shopping Cart
+            {showCartBadge()}
+          </Link>
+        </nav>
+
+      </div>
+    </header>
+  );
+}
+
+export default Header;
