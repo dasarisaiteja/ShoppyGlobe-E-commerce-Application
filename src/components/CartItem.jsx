83a7d8534577bbe592adf740117e2ad92b7b3928
dasarisaiteja <dasarisaiteja210@gmail.com>
2026-02-11 20:21:54 +0530

    created cart component

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,5 @@
 import { useDispatch } from 'react-redux';
-// We need these to change our Redux state
 import { removeFromCart, updateQuantity } from '../utils/cartSlice';
-import './CartItem.css';
 
 // Using props as the parameter so we can see where the data comes from
 function CartItem(props) {
@@ -10,14 +8,11 @@ function CartItem(props) {
   // Making a simple variable for our item
   const myItem = props.item;
 
-  // 1. This function adds 1 to the current quantity
   function plusOne() {
     const newNumber = myItem.quantity + 1;
-    // We send the ID and the new quantity to Redux
     dispatch(updateQuantity({ id: myItem.id, quantity: newNumber }));
   }
 
-  // 2. This function subtracts 1 but stops at 1
   function minusOne() {
     if (myItem.quantity > 1) {
       const newNumber = myItem.quantity - 1;
@@ -27,14 +22,12 @@ function CartItem(props) {
     }
   }
 
-  // 3. This function removes the item completely
+  //  This function removes the item completely
   function deleteItem() {
-    // We only need the ID to find which one to remove
     dispatch(removeFromCart(myItem.id));
   }
 
   // Calculate the subtotal for this specific item
-  // Beginners like to do the math here rather than inside the HTML tags
   const itemTotalPrice = myItem.price * myItem.quantity;
 
   return (
