89fab80b919c2b63c2488804cb98663490c4bee4
dasarisaiteja <dasarisaiteja210@gmail.com>
2026-02-11 20:35:27 +0530

    updated app and main component for project load

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { useEffect } from "react"
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './components/header';
+import './App.css';
 
 function App() {
   
-  useEffect()
+  const loadingScreen = (
+    <div className="loading-screen">
+      <h2>Please wait...</h2>
+      <p>Our shop is loading!</p>
+    </div>
+  );
 
   return (
-    <>
-      
-    </>
-  )
+    <div className="shoppyglobe-app">
+      <Header />
+
+      <main className="content-area">
+        <Suspense fallback={loadingScreen}>
+          <Outlet />
+        </Suspense>
+      </main>
+      <footer className="my-footer">
+        <p>Copyright 2024 - ShoppyGlobe E-commerce Project</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
