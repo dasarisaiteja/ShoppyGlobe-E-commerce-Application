eb58bb539fb0c233350fe3e686bfadc5830a39e0
dasarisaiteja <dasarisaiteja210@gmail.com>
2026-02-25 23:35:52 +0530

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'ShoppyGlobe-E-commerce-Application',
+  base: '/ShoppyGlobe-E-commerce-Application/',
   plugins: [react()],
 })
